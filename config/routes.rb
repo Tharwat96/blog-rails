32457f9e1c68bfecad262d488ead95e6ab4a1d03
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-03-29 03:08:04 +0200

    added initial models and changed to ruby 2.6.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
+  get 'posts/index'
+  get 'posts/new', to: 'posts#new'
+  root to: 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
