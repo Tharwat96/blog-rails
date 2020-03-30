b3e8a7bb68de3325238e1a6dacb7eddbc3cbb1b2
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-03-30 15:02:56 +0200

    added show page and new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = 1
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(id=params[:id])
   end
 
   def update
-
   end
 
   def delete
 
+  end
+
+  private
+  def post_params
+    params.permit(:title, :content)
   end
 end
